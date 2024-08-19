38b35874e68d264e23f5f6774e87ec47059da069
vojtechrichter <vojta3218@gmail.com>
2024-08-19 08:58:40 +0200

    reset

--- arena.h
+++ arena.h
@@ -1,31 +1,6 @@
 #ifndef ARENA_H_
 #define ARENA_H_
 
-// maybe infer this from os specific compiler directives?
 
-#define MALLOC_BACKEND 0
-#define VIRTUAL_ALLOC_BACKEND 0
-
-#ifndef ALLOC_BACKEND
-#define ALLOC_BACKEND MALLOC_BACKEND
-#endif
-
-#include <stdio.h>
-#include <assert.h>
-
-#if ALLOC_BACKEND == MALLOC_BACKEND
-	#include <stdlib.h>
-#endif
-
-typedef struct {
-	size_t capacity;
-	size_t size;
-	char *mem;
-} Arena;
-
-Arena arena_init(size_t capacity);
-char *arena_alloc(Arena *arena, size_t alloc_size);
-void arena_reset(Arena *arena);
-void arena_free(Arena *arena);
 
 #endif
--- main.c
+++ main.c
@@ -4,19 +4,5 @@
 
 int main(void)
 {
-	Arena arena = arena_init(10 * sizeof(int));
-
-	int *arr = (int *)arena_alloc(&arena, 5 * sizeof(int));
-
-	for (int i = 0; i < 5; i++) {
-		arr[i] = (i * 2) + (i * i / 5);
-	}
-	
-	for (int i = 0; i < 5; i++) {
-		printf("%d\n", arr[i]);
-	}
-
-	arena_free(&arena);
-
 	return 0;
 }
