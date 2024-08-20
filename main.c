6d879eec625800262c42aff482367598696ffefc
vojtechrichter <vojta3218@gmail.com>
2024-08-20 09:35:45 +0200

    testing the allocation

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
-#define ALLOC_BACKEND MALLOC_BACKEND
+#define ALLOC_BACKEND VIRTUAL_ALLOC_BACKEND
 
 #include "arena.h"
 
 int main(void)
 {
+	ArenaAllocator *a = arena_init(40);
+	printf("%zd\n", 10 * sizeof(int));
+
+	int *array = (int *)arena_push(a, 10 * sizeof(int));
+
+	for (int i = 0; i < 10; i++) {
+		array[i] = (i + 1) * (i + 2);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", array[i]);
+	}
+
+	arena_reset(a);
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", array[i]);
+	}
+
 	return 0;
 }
